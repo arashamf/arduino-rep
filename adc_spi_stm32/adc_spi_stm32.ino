cef146cdeee5695393ad8e9ee0aa54efaeb40174
aleksey <arashamph@gmail.com>
2021-07-26 17:36:56 +0300

    edit adc_spi_stm32 projects

--- adc_spi_stm32/adc_spi_stm32.ino
+++ adc_spi_stm32/adc_spi_stm32.ino
@@ -4,9 +4,13 @@
 #define DHTPIN 2     // номер пина, к которому подсоединен датчик
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 
+#define MOSI 11 
+#define MISO 12 
+#define SCK  13
+#define SS 10
+
 DHT dht(DHTPIN, DHTTYPE);     // Инициализация сенсора DHT
 
-unsigned short sensor_pin = A0; 
 float value_humidity = 0; //значение влажности
 float value_temp = 0; //значение температуры
 unsigned short output_value [5]; //массив со значениями АЦП
@@ -14,19 +18,24 @@ uint8_t count = 0;
 
 ISR(SPI_STC_vect) 
 {
-unsigned char br = SPDR;
+unsigned char br = SPDR; //SPDR - принятый по SPI байт
+Serial.println (br);
 if (br == 0xCA)
   {
-    SPI.transfer (output_value [count]);
+    SPI.transfer (0xAB);
   }
+  else
+    SPI.transfer (0x55);
 }
 
 void setup() {
   Serial.begin(57600);
+//  Serial.println("I am Master");
   Serial.println("Reading From the Sensor ...");
   
+ // digitalWrite(SS, HIGH);
   SPI.begin(); //  Инициализируем SPI при SS в режиме INPUT (получаем slave режим) и вручную выставляем правильные направления пинов
-  SPI.beginTransaction (SPISettings()); //SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0))
+  SPI.beginTransaction (SPISettings(6000000, MSBFIRST, SPI_MODE0)); //12 МГц, передача данных идёт, начиная с MSB, CPOL=0, CPHA=0
   pinMode(MISO, OUTPUT);
   pinMode(MOSI, INPUT);
   pinMode(SCK, INPUT);
@@ -39,14 +48,6 @@ void setup() {
 }
 
 void loop() {
-/*  for (count = 0; count < 5; count++)
-    {
-    output_value [count] = analogRead(sensor_pin);
- // output_value = map(output_value,1023,0,0,100); //map(value, fromLow, fromHigh, toLow, toHigh)
-    Serial.print("VALUE : ");
-    Serial.println(output_value [count]);
-    delay(500); 
-    }*/
 value_humidity = dht.readHumidity();    // cчитывание влажности
 value_temp = dht.readTemperature();     // cчитывание температуры
 
@@ -63,6 +64,12 @@ else
   Serial.print ("Temperature: ");
   Serial.print (value_temp);
   Serial.println (" *C");
-  delay(1000);
+
+/*  SPI.beginTransaction (SPISettings(6000000, MSBFIRST, SPI_MODE0)); //6 МГц, передача данных идёт, начиная с MSB, CPOL=0, CPHA=0
+  digitalWrite(SS, LOW);
+  SPI.transfer(0xAB);
+  digitalWrite(SS, HIGH);
+  SPI.endTransaction();*/
   }
+  delay(1500);
 }
